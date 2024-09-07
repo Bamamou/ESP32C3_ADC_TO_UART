2a1d38acbb3914a9e878e33c9df25f96fd3b823a
CecedeKP <bamamounicolas@gmail.com>
2024-09-07 16:45:08 +0800

    Sending the reading data via UART added

---
+++ include/global_Constants.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * global_Constants.h
+ * Constants that will be used in the whole project
+ *****************************************************************************/
+#ifndef __GLOBAL_CONSTANTS_H__
+#define __GLOBAL_CONSTANTS_H__
+
+#include <Arduino.h>
+
+#define UART_RX_PIN 20     // GPIO for UART RX 
+#define UART_TX_PIN 21    // GPIO for UART TX 
+
+
+/* #endregion */
+#endif
---
+++ include/main.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * main.h
+ * Header for the main file
+ *****************************************************************************/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <Arduino.h>
+#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "global_Constants.h"
+
+
+void taskADC(void *pvParameters);
+
+#endif
